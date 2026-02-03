a205a1683a6f10188cdbbf04782c6e4f00c7f9bf
Rajesh Prasad <130226822+Rajeshpd339@users.noreply.github.com>
2026-02-03 21:47:21 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,35 @@
+# n1=int(input("N1="))
+# n2=int(input("N2="))
+# if n1>n2:
+#     print("n1 is greater ")
+# else:
+#     print("n2 is greater ")
+#2.Write a program to Find the greatest among three numbers.
+# n1=int(input("N1="))
+# n2=int(input("N2="))
+# n3=int(input("N3="))
+# if n1>n2 and n1>n3:
+#     print("n1 is greater value ")
+# elif n2>n3:
+#     print("n2 is greater value ")
+# else:
+#     print("n3 is greater value ")
+#3.Write a program to Check whether a given number is positive or negative.
+# n1=int(input("N1="))
+# if n1>0:
+#     print("+ve")
+# else:
+#     print("-ve")
+
+#4.Write a program to Check whether a number is even or odd.
+# n1=int(input("N1="))
+# if n1%2==0:
+#     print("even number ")
+# else:
+#     print("odd number ")
+#5.Write a program to Check whether a person is eligible to vote or not.
+age=int(input("Enter the age "))
+if age>=18:
+    print("vote de skte hai ")
+else:
+    print("rajnitik pr bat n kre")
