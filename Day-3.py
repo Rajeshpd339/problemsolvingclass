b1b2ddac2747c9cf9eed9996f6325870c8f8fffb
Rajesh Prasad <130226822+Rajeshpd339@users.noreply.github.com>
2026-02-04 22:24:57 +0530

    Add files via upload

---
+++ Day-3.py
@@ -0,0 +1,28 @@
+#1.Write a program to print number from 1 to 100.
+
+# for i in range(1,1001):
+#     # print(i,end=" ")
+#     print("unknow  jan paran...",end=" ")
+
+# for mohan in range(1,101):
+#     if mohan%2==0:
+#         print(mohan)
+#3.Write a program to find the sum of natural number
+# n=int(input("Enter the number "))
+# s=0
+# for i in range(1,n+1):
+#     s=s+i 
+#     # print(s)
+# print("sum of natural number=",s)
+#14Write a program to find the sum of all even number from the given number 
+# s=0
+# for mohan in range(1,101):
+#     # s=s+mohan
+#     if mohan%2==0:
+#         s=s+mohan
+#         # print(mohan)
+# print("even sum=",s)
+#15. Write a program to print the number that is divisible by 3 and 5  1 to 100
+for i in range(1, 101):
+    if i%3==0 and i%5==0:
+        print(i)
